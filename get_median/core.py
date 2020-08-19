25f20aacb0c287169075be3ec9a768f2f8ce1c8f
Rishabh Thurkal <rishi.feb1@gmail.com>
2020-08-19 12:27:31 +0200

    Added get_median module which houses the pipeline functionality

---
+++ get_median/core.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: rish
+# @Date:   2020-08-19 12:22:27
+# @Last Modified by:   rish
+# @Last Modified time: 2020-08-19 12:27:03
+
+### Imports START
+
+### Imports END
+
+
+# [START Function to calculate median and create output]
+def get_median_for_sesnors():
+	pass
+# [END]
---
+++ get_median/utils.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: rish
+# @Date:   2020-08-19 12:22:30
+# @Last Modified by:   rish
+# @Last Modified time: 2020-08-19 12:26:53
